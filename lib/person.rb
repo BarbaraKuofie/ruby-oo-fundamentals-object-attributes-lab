a48495f08174c7d283dd281292c627a782b19604
BarbaraKuofie <barbara.kuofie@gmail.com>
2020-09-15 16:27:41 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,25 @@
+class Person
+    def name(name)
+        @name = name 
+    end 
+
+    def name 
+        @name 
+    end 
+
+    def name=(updated_name)
+        @name = updated_name
+    end 
+
+    def jon(job)
+        @job = job 
+    end 
+
+    def job 
+        @job 
+    end 
+
+    def job=(new_job)
+        @job = new_job
+    end 
+end 
